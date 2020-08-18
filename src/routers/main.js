7d451628f962caa09d2b6c480c0675cc2f78e8c4
Xignotic <38007603+Xignotic84@users.noreply.github.com>
2020-08-18 08:50:01 +0800

    example env update

--- src/routers/main.js
+++ src/routers/main.js
@@ -6,7 +6,8 @@ Router.get('/', (req, res, next) => {
 
   // Render page with data
   res.render('pages/index.ejs', {
-    pagetitle: 'Home'
+    pagetitle: 'Home',
+    user: req.session.user || false
   })
 
   next()
