345cc2c4bf3d1406f2c3f7a9203203be79c448b9
Xignotic <xignotic84@gmail.com>
2020-10-08 16:49:25 +0800

    css  changes

--- src/routers/main.js
+++ src/routers/main.js
@@ -4,13 +4,15 @@ const Router = express.Router()
 // Listen to endpoints on this route
 Router.get('/', async (req, res, next) => {
 
-  const quizzes = await req.db.find(4, {})
+  const quizzes = await req.db.find(4, {is_public: true})
+  const groups = await req.db.find(3, {is_public: true})
 
   // Render page with data
   res.render('pages/index.ejs', {
     pagetitle: 'Home',
     user: req.session.user || false,
     quizzes: quizzes || [],
+    groups: groups || []
   })
 
   next()
